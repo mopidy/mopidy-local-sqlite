a4061c7a333800b6ba803fcd47e679134d988183
Thomas Kemmer <tkemmer@computer.org>
2015-06-17 19:33:38 +0200

    Initial py.test cases.

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,23 +1,20 @@
 from __future__ import unicode_literals
 
-import unittest
-
 from mopidy_local_sqlite import Extension
 
 
-class ExtensionTest(unittest.TestCase):
+def test_get_default_config():
+    ext = Extension()
+    config = ext.get_default_config()
+    assert '[local-sqlite]' in config
+    assert 'enabled = true' in config
 
-    def test_get_default_config(self):
-        ext = Extension()
-        config = ext.get_default_config()
-        self.assertIn('[local-sqlite]', config)
-        self.assertIn('enabled = true', config)
 
-    def test_get_config_schema(self):
-        ext = Extension()
-        schema = ext.get_config_schema()
-        self.assertIn('directories', schema)
-        self.assertIn('timeout', schema)
-        self.assertIn('use_album_mbid_uri', schema)
-        self.assertIn('use_artist_mbid_uri', schema)
-        self.assertIn('search_limit', schema)
+def test_get_config_schema():
+    ext = Extension()
+    schema = ext.get_config_schema()
+    assert 'directories' in schema
+    assert 'timeout' in schema
+    assert 'use_album_mbid_uri' in schema
+    assert 'use_artist_mbid_uri' in schema
+    assert 'search_limit' in schema
